313993b6ed23c1a6c032bc3868c1f248f5d31842
Pillow-chan <strangewrongname@gmail.com>
2021-07-10 16:59:17 +0700

    Errors Correction

--- Services/FakeTodoItemService.cs
+++ Services/FakeTodoItemService.cs
@@ -2,10 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
 
 namespace AspNetCoreTodo.Services
 {
-    public class FakeTodoItemService
+    public class FakeTodoItemService : ITodoItemService
     {
+        public Task<TodoItem[]> GetIncompleteItemsAsync()
+        {
+            var item1 = new TodoItem
+            {
+                Title = "Learn ASP.NET Core",
+                DueAt = DateTimeOffset.Now.AddDays(1)
+            };
+            var item2 = new TodoItem
+            {
+                Title = "Build awesome(?) apps",
+                DueAt = DateTimeOffset.Now.AddDays(2)
+            };
+            var item3 = new TodoItem
+            {
+                Title = "Or at least something what will work",
+                DueAt = DateTimeOffset.Now.AddDays(2)
+            };
+            return Task.FromResult(new[] { item1, item2, item3 });
+        }
     }
 }
--- Services/ITodoItemService.cs
+++ Services/ITodoItemService.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
 
 namespace AspNetCoreTodo.Services
 {
-    interface ITodoItemService
+    public interface ITodoItemService
     {
+        Task<TodoItem[]> GetIncompleteItemsAsync();
     }
 }
